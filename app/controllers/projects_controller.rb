704d3e0f2242426618184296b96cf66d995d5dc5
Seung Koh <skoh@uw.edu>
2012-01-21 20:22:53 -0800

    Creation of projects feature complete

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,20 @@
+class ProjectsController < ApplicationController
+  def index
+  end
+  
+  def new
+    @project = Project.new
+  end
+  
+  def create
+    @project = Project.new(params[:project])
+    @project.save
+    redirect_to @project, :notice => 'Project has been created.'
+    #flash[:notice] = 'Project has been created.'
+    #p params
+  end
+  
+  def show
+    @project = Project.find(params[:id])
+  end
+end
