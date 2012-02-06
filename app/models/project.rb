1f891e5a22314dee037483c18b71b28f9017021f
Seung Koh <skoh@uw.edu>
2012-02-05 21:28:30 -0800

    Made projects only visible to users with permission to see them

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,9 @@
 class Project < ActiveRecord::Base
+  # Commented out following option from name field validation because it gives me "Validation failed: Name has already been taken" error: :uniqueness => true
   validates :name, :presence => true
   has_many :tickets, :dependent => :delete_all
+  has_many :permissions, :as => :thing
+  scope :readable_by, lambda { |user|
+    joins(:permissions).where(:permissions => { :action => "view", :user_id => user.id })
+  }
 end
